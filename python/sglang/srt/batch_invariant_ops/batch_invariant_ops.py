a2b41819b8cf4955251bba3a88cb27c10aafb981
fzyzcjy <ch271828n@outlook.com>
2025-10-26 08:48:37 +0800

    more

--- python/sglang/srt/batch_invariant_ops/batch_invariant_ops.py
+++ python/sglang/srt/batch_invariant_ops/batch_invariant_ops.py
@@ -10,13 +10,14 @@ import triton
 import triton.language as tl
 
 from sglang.srt.layers.deep_gemm_wrapper.configurer import ENABLE_JIT_DEEPGEMM
-from sglang.srt.utils.common import calc_diff
-from sglang.srt.utils.common import get_bool_env_var
+from sglang.srt.utils.common import calc_diff, get_bool_env_var
 
 if ENABLE_JIT_DEEPGEMM:
     import deep_gemm
 
-_ENABLE_MM_COMPARISON_TEST = get_bool_env_var("SGLANG_BATCH_INVARIANT_OPS_ENABLE_MM_COMPARISON_TEST")
+_ENABLE_MM_COMPARISON_TEST = get_bool_env_var(
+    "SGLANG_BATCH_INVARIANT_OPS_ENABLE_MM_COMPARISON_TEST"
+)
 
 __all__ = [
     "set_batch_invariant_mode",
@@ -246,26 +247,29 @@ def _matmul_persistent_deepgemm(
 def matmul_persistent(
     a: torch.Tensor, b: torch.Tensor, bias: torch.Tensor | None = None
 ):
-    # DeepGEMM does not support fp32 gemm
-    if (not ENABLE_JIT_DEEPGEMM) or (a.dtype == torch.float32):
-        return _matmul_persistent_triton(a=a, b=b, bias=bias)
+    if (
+        ENABLE_JIT_DEEPGEMM
+        and (a.dtype == torch.bfloat16)
+        and (b.dtype == torch.bfloat16)
+    ):
+        if _ENABLE_MM_COMPARISON_TEST:
+            out_triton = _matmul_persistent_triton(a=a, b=b, bias=bias)
+            out_deepgemm = _matmul_persistent_deepgemm(a=a, b=b, bias=bias)
+            diff = calc_diff(out_triton, out_deepgemm)
+            assert diff < 0.0001, f"{diff=} {out_triton=} {out_deepgemm=}"
+            # can be enabled for debugging
+            # print(
+            #     f"{diff=} "
+            #     f"{(out_triton - out_deepgemm).abs().mean()=} "
+            #     f"{(out_triton - out_deepgemm).abs().sum()=} "
+            #     f"{torch.sum(out_triton != out_deepgemm)=} "
+            # )
+            # print(f"{a=} {b=} {bias=} {out_triton=} {out_deepgemm=}")
+            return out_deepgemm
 
-    if _ENABLE_MM_COMPARISON_TEST:
-        out_triton = _matmul_persistent_triton(a=a, b=b, bias=bias)
-        out_deepgemm = _matmul_persistent_deepgemm(a=a, b=b, bias=bias)
-        diff = calc_diff(out_triton, out_deepgemm)
-        assert diff < 0.0001, f"{diff=} {out_triton=} {out_deepgemm=}"
-        # can be enabled for debugging
-        # print(
-        #     f"{diff=} "
-        #     f"{(out_triton - out_deepgemm).abs().mean()=} "
-        #     f"{(out_triton - out_deepgemm).abs().sum()=} "
-        #     f"{torch.sum(out_triton != out_deepgemm)=} "
-        # )
-        # print(f"{a=} {b=} {bias=} {out_triton=} {out_deepgemm=}")
-        return out_deepgemm
+        return _matmul_persistent_deepgemm(a=a, b=b, bias=bias)
 
-    return _matmul_persistent_deepgemm(a=a, b=b, bias=bias)
+    return _matmul_persistent_triton(a=a, b=b, bias=bias)
 
 
 @triton.jit
